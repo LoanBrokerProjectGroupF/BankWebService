1eba3922cb1cc61740f88922a479e4417d668d75
darkSideMatters <plamengetsov95@gmail.com>
2018-11-20 14:36:57 +0100

    clean up

--- src/main/java/groupfbank_json/RestService.java
+++ src/main/java/groupfbank_json/RestService.java
@@ -16,7 +16,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
-import static javax.ws.rs.client.Entity.json;
 import javax.ws.rs.core.MediaType;
 
 /**
